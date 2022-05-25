eae76a98e44fb84b1850b8f7e7413d6cc2430e6b
ashes <792634614@qq.com>
2022-05-25 20:32:33 +0800

    test split

---
+++ src/type.ts
@@ -0,0 +1,17 @@
+import { JuggleDV } from '@juggle-data-view/types';
+/**
+ * 组件属性类型
+ * @创建时间 2020-06-30 9:00:00
+ */
+
+/**
+  option: { echarts: any; };
+ * Step1: 声明组件模板配置的类型
+ */
+export interface Config extends JuggleDV.Config {}
+
+// for Index.tsx
+export type IIndex = JuggleDV.CompIndex<Config>;
+
+// for temps/{config}.ts
+export type IConfig = JuggleDV.CompConfig<Config>;
